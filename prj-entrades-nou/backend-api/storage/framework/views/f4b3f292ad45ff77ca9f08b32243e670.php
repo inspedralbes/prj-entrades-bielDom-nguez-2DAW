8956f20b3c411bcf4db02c2b7a3407490ea02a77
bieldominguez-acceleralia <biel.dominguez@acceleralia.com>
2026-04-13 20:24:19 +0200

    Initial setup of project files and directory structure.

---
+++ prj-entrades-nou/backend-api/storage/framework/views/f4b3f292ad45ff77ca9f08b32243e670.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH C:\Users\bield\Documents\2-Programacio\2-COLE\2-DAW\2.ProjectesTransversals\3-Tr3\prj-entrades-nou\backend-api\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
